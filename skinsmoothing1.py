38ae22e897623a46a4adc76da84819e1efdea756
weinan030416 <86523415+Null-wn@users.noreply.github.com>
2022-09-11 07:51:57 +0800

    Add files via upload

---
+++ skinsmoothing1.py
@@ -0,0 +1,108 @@
+import tkinter as tk
+import tkinter.messagebox
+from tkinter.constants import FALSE, FLAT
+import tkinter.filedialog
+from PIL import Image, ImageTk
+import numpy as np
+import cv2
+import cv2 as cv
+
+
+def Showimage(img1,img2,canva1,canva2):
+    global imgTK1
+    canvawidth = int(canva1.winfo_reqwidth())
+    canvaheight = int(canva1.winfo_reqheight())
+    sp = img1.shape
+    cvheight = sp[0]#height(rows) of image
+    cvwidth = sp[1]#width(colums) of image
+    if (float(cvwidth/cvheight) > float(canvawidth/canvaheight)):
+        imgCV = cv.resize(img1,(canvawidth,int(canvawidth*cvheight/cvwidth)), interpolation=cv.INTER_AREA)
+    else:
+        imgCV = cv.resize(img1,(int(canvaheight*cvwidth/cvheight),canvaheight), interpolation=cv.INTER_AREA)
+    imgCV2 = cv.cvtColor(imgCV, cv.COLOR_BGR2RGBA)#转换颜色从BGR到RGBA
+    current_image = Image.fromarray(imgCV2)#将图像转换成Image对象
+    imgTK1 = ImageTk.PhotoImage(image=current_image)#将image对象转换为imageTK对象
+    canva1.create_image(150,150,image = imgTK1,anchor='center')
+
+    global imgTK2
+    canvawidth = int(canva2.winfo_reqwidth())
+    canvaheight = int(canva2.winfo_reqheight())
+    sp = img2.shape
+    cvheight = sp[0]#height(rows) of image
+    cvwidth = sp[1]#width(colums) of image
+    if (float(cvwidth/cvheight) > float(canvawidth/canvaheight)):
+        imgCV = cv.resize(img2,(canvawidth,int(canvawidth*cvheight/cvwidth)), interpolation=cv.INTER_AREA)
+    else:
+        imgCV = cv.resize(img2,(int(canvaheight*cvwidth/cvheight),canvaheight), interpolation=cv.INTER_AREA)
+    imgCV2 = cv.cvtColor(imgCV, cv.COLOR_BGR2RGBA)#转换颜色从BGR到RGBA
+    current_image = Image.fromarray(imgCV2)#将图像转换成Image对象
+    imgTK2 = ImageTk.PhotoImage(image=current_image)#将image对象转换为imageTK对象
+    canva2.create_image(150,150,image = imgTK2,anchor='center')
+
+def skinSmoothing1(img,level):
+    dst = np.zeros_like(img)
+    v1 = level
+    v2 = 1
+    dx = v1 * 5 
+    fc = v1 * 12.5 
+    p = 0.1
+    temp4 = np.zeros_like(img)
+    temp1 = cv2.bilateralFilter(img,dx,fc,fc)
+    temp2 = cv2.subtract(temp1,img);
+    temp2 = cv2.add(temp2,(10,10,10,128))
+    temp3 = cv2.GaussianBlur(temp2,(2*v2 - 1,2*v2-1),0)
+    temp4 = cv2.add(img,temp3)
+    dst = cv2.addWeighted(img,p,temp4,1-p,0.0)
+    dst = cv2.add(dst,(10, 10, 10,255))
+    return dst
+
+def SaveAs():
+    global dst
+    img=dst
+    path = tk.filedialog.asksaveasfilename()
+    cv2.imwrite(path + ".jpg", img)
+    if path:
+        tkinter.messagebox.showinfo(title='保存成功', message="已成功保存到：" + path + ".jpg")
+    else:
+        tkinter.messagebox.showwarning(title='保存失败', message="保存失败，请检查保存路径")
+
+def level(level):
+    global dst
+    if level=='1':
+        dst=dst1
+    elif level=='2':
+        dst=dst2
+    elif level=='3':
+        dst=dst3
+    elif level=='4':
+        dst=dst4
+    else:
+        dst=dst5
+    Showimage(src,dst,canva1,canva2)
+
+root = tk.Tk()
+root.geometry('640x410+600+300')
+root.title("磨皮美白")
+canva1 = tk.Canvas(root, width=300, height=300,bg="white")
+canva1.place(x=5,y=45)
+canva2 = tk.Canvas(root, width=300, height=300,bg="white")
+canva2.place(x=330,y=45)
+label1 = tk.Label(root, text='Source', font=('Arial Black', 12), width=15, height=1 )
+label1.place(x=155,y=15,anchor='n')
+label2 = tk.Label(root, text='Destination', font=('Arial Black', 12), width=15, height=1 )
+label2.place(x=480,y=15,anchor='n')
+btn_SaveAs = tk.Button(root, text="保存", padx=20, pady=1.5, font=('Arial', 12), command=SaveAs)  # 保存
+btn_SaveAs.place(x=580, y=380, anchor="center")
+scale=tkinter.Scale(root,from_=1,to=5,orient=tkinter.HORIZONTAL,length=430,showvalue=1,tickinterval=1,resolution=1,command=level)
+scale.place(x=85, y=380, anchor="w")
+label=tkinter.Label(root,text='磨皮程度',anchor='center',font=('Arial', 10),width=10,height=1)
+label.place(x=5, y=380, anchor="w")
+path = tk.filedialog.askopenfilename()
+src=cv2.imread(path)
+dst1 = skinSmoothing1(src,2)
+dst2 = skinSmoothing1(src,4)
+dst3 = skinSmoothing1(src,6)
+dst4 = skinSmoothing1(src,8)
+dst5 = skinSmoothing1(src,10)
+Showimage(src,dst1,canva1,canva2)
+root.mainloop()
