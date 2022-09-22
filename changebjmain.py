c2a0c659296bfa501954c5c17644bf0ec78d7caf
weinan030416 <86523415+Null-wn@users.noreply.github.com>
2022-09-22 18:01:20 +0800

    Delete changebjmain.py

--- changebjmain.py
+++
@@ -1,8 +0,0 @@
-#抠图
-import os, paddlehub as hub
-
-humanseg = hub.Module(name='deeplabv3p_xception65_humanseg')  # 加载模型
-path = 'D:\dachuang\makeup\imgs\Korea\\'  # 文件目录
-files = [path + i for i in os.listdir(path)]  # 获取文件列表
-print(files)
-results = humanseg.segmentation(data={'image': files})  # 抠图
